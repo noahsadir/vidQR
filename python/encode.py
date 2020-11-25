ea61684af716a6931852fe641a3742b73cb8fe83
Noah Sadir <development.noahsadir@gmail.com>
2020-11-25 17:07:28 -0500

    cleaned up encoder

--- python/encode.py
+++ python/encode.py
@@ -1,12 +1,11 @@
+#Packages
 import argparse
 import qrcode
 import cv2
 import os
-import codecs
 import imageio
 from PIL import Image
 from barcode import Code128
-from barcode import EAN13
 from barcode.writer import ImageWriter
 from pyzbar import pyzbar
 from pyzbar.pyzbar import ZBarSymbol
@@ -17,16 +16,15 @@ import time
 import sys
 
 parser = argparse.ArgumentParser(description='Retrieve options dates for a given stock')
-parser.add_argument('-p', '--path',default="",help="File Path")
-parser.add_argument('-o', '--output',default="data/data.gif",help="File Path")
-parser.add_argument('-v', '--version',default="10",help="File Path")
-parser.add_argument('-e', '--errcorrect',default="0",help="File Path")
-parser.add_argument('-f', '--fps',default="10",help="File Path")
-parser.add_argument('-b', '--binary',default="true",help="File Path")
-parser.add_argument('-fl', '--flash',default="true",help="File Path")
+parser.add_argument('-p', '--path',default="",help="**REQUIRED** Input Path")
+parser.add_argument('-o', '--output',default="vidqr.gif",help="Output Path [optional; default {WORKING_DIR}/vidqr.gif]")
+parser.add_argument('-v', '--version',default="10",help="Version btwn 1 and 40 [optional, default 10]")
+parser.add_argument('-e', '--errcorrect',default="0",help="Error correction level btwn 0 (low) and 3 (high) [optional; default 0]")
+parser.add_argument('-f', '--fps',default="5",help="Framerate [optional; defualt 5]")
+parser.add_argument('-fl', '--flash',default="false",help="Animation flashes at every loop [optional; default false]")
 args = parser.parse_args()
 
-
+#Number of bytes that can be stored for each QR code version at each err correction level
 capacity = [[17,14,11,7],
 [32,26,20,14],
 [53,42,32,24],
@@ -68,116 +66,35 @@ capacity = [[17,14,11,7],
 [2809,2213,1579,1219],
 [2953,2331,1663,1273]]
 
-def binToAscii(binary):
-    ascii = ""
-    for binaryItem in binary:
-        ascii = ascii + chr(int(binaryItem))
-    return ascii
-
-def blackAndWhite(img,th=127):
-    if img is not None:
-        originalImage = img
-        grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
-        (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, th, 255, cv2.THRESH_BINARY)
-        return blackAndWhiteImage
-    return img
-
-def readPacketID(img,th=127):
-    img = blackAndWhite(img,th)
-    barcodes = pyzbar.decode(Image.fromarray(img), symbols=[ZBarSymbol.EAN13])
-    if len(barcodes) >= 1:
-        return int(barcodes[0].data.decode('utf-8'))
-    return 0
-
-def barcodeOfInt(intVal):
-    qrc = qrcode.QRCode(version=1,error_correction=errCorrectConstant)
-    qrc.add_data(str(intVal))
-    qrc.make()
-    qrc.make_image().save("tmp/bc" + str(intVal) + ".png")
-    #number = str(intVal)
-    #for i in range(len(number),12):
-        #number =  "0" + number
-    #my_code = EAN13(number, writer=ImageWriter())
-    #my_code.save("tmp/bc" + str(intVal), options={"write_text": False})
-
-def genQR(data):
-    qr = qrcode.QRCode(version=versionOfCode,error_correction=errCorrectConstant)
-    qr.add_data(data)
-    qr.make()
-    return qr.make_image()
-
-def percentageBar(value,max):
-    outputString = "["
-    percent = round((value / max) * 100)
-    ticks = round(percent / 2)
-    for index in range(0,50):
-        if index < ticks:
-            outputString = outputString + "*"
-        else:
-            outputString = outputString + " "
-    outputString = outputString + "] " + str(percent) + "%"
-    return outputString
-
-def joinImages(qrCode, barCode, interpolation=cv2.INTER_CUBIC):
-    w_min = qrCode.shape[1]
-    barCode = cv2.resize(barCode, (800,100), interpolation=cv2.INTER_CUBIC)
-    qrCode = cv2.resize(qrCode, (800,800), interpolation=cv2.INTER_CUBIC)
-    #index = cv2.resize(index, (int(index.shape[0] * w_min / qrCode.shape[1]), int(index.shape[0] * w_min / qrCode.shape[1])), interpolation=cv2.INTER_CUBIC)
-    #barCode = cv2.resize(barCode, (index.shape[0] - int(barCode.shape[0] * w_min / qrCode.shape[1]), int(barCode.shape[0] * w_min / qrCode.shape[1])), interpolation=interpolation)
-    #qrCode = cv2.resize(qrCode, (w_min, int(qrCode.shape[0] * w_min / qrCode.shape[1])), interpolation=interpolation)
-
-    return cv2.vconcat([qrCode,barCode])
-
 startTime = time.time()
+permanentPrint = "" #Text intended to remain after clearing console
+
+#Binary data of input file stored as a Base64 string
 paragraph = ""
+
 filetype = "txt"
-extension = os.path.splitext(args.path)[1].replace(".","")
+
+#METADATA - Stored in Code128 Barcode
+extension = os.path.splitext(args.path)[1].replace(".","") #Store extension of input file for decoding
 versionOfCode = int(args.version)
 errorCorrection = int(args.errcorrect)
+originalSize = os.path.getsize(args.path)
+framerate = int(args.fps)
 if errorCorrection < 0:
     errorCorrection = 0
 elif errorCorrection > 3:
     errorCorrection = 3
 
-print(extension)
-bin_data = open(args.path, 'rb').read()
-filetype = "bin"
-
-d = "".join(map(chr, bin_data))
-paragraph = binascii.b2a_base64(bin_data)
-
-file = open('data/bin.txt', "wb")
-file.write(paragraph)
-file.close()
-
-bytesPerCode = capacity[versionOfCode - 1][errorCorrection] - 8
-totalBytes = len(paragraph)
-numberOfCodes = math.ceil(totalBytes / bytesPerCode)
-
-print("File size: " + str(totalBytes) + " Bytes")
-print("Bytes per code: " + str(bytesPerCode))
-print("Number of codes: " + str(numberOfCodes))
-print("\n---------------------\n")
-file = open('data/raw.txt', "wb")
-file.write(paragraph)
-file.close()
-
-x = bytesPerCode
-print("Capacity: " + str(x) + " Bytes")
-
-
-
-splitValues = [paragraph[i: i + x] for i in range(0, len(paragraph), x)]
-print(splitValues)
-for splitValue in splitValues:
-    print(len(splitValue))
-count = 0
-images = []
-framerate = int(args.fps)
-version = 3
-
-originalSize = os.path.getsize(args.path)
-
+#CONFIGURATION - Used to encode data
+flashes = False
+version = int(args.fps)
+if args.flash == "true":
+    flashes = True
+#Ensure version is between 1 and 40
+if version < 1:
+    version = 1
+elif version > 40:
+    version = 40
 errCorrectConstant = qrcode.constants.ERROR_CORRECT_L
 if errorCorrection == 1:
     qrcode.constants.ERROR_CORRECT_M
@@ -186,82 +103,161 @@ elif errorCorrection == 2:
 elif errorCorrection == 3:
     qrcode.constants.ERROR_CORRECT_H
 
-barcodeData = str(framerate) + ":" + str(len(splitValues)) + ":" + extension + ":" + str(originalSize)
-number = barcodeData
-my_code = Code128(number, writer=ImageWriter())
-my_code.save("data/barcode", options={"write_text": False})
-barcode = cv2.imread("data/barcode.png")
-barcode = cv2.resize(barcode, (barcode.shape[1], int(barcode.shape[0] / 4)), interpolation=cv2.INTER_CUBIC)
+#Generate Code128 barcode with string data
+def generateCode128(dataString):
+    code128Barcode = Code128(dataString, writer=ImageWriter())
+    #TODO: Directly save barcode to variable instead of writing then reading
+    code128Barcode.save("tmp/barcode", options={"write_text": False})
+    img = cv2.imread("tmp/barcode.png")
+    os.remove("tmp/barcode.png")
+    return img
 
-qrco = qrcode.QRCode(version=1,error_correction=errCorrectConstant)
-qrco.add_data("NULL")
-qrco.make()
-qrco.make_image().save("tmp/bc_empty.png")
+#Generate QR code with desired data
+def generateQR(data):
+    global versionOfCode, errCorrectConstant
+    qr = qrcode.QRCode(version=versionOfCode,error_correction=errCorrectConstant)
+    qr.add_data(data)
+    qr.make()
+    pilQR = qr.make_image()
+    pilQR.save("tmp/qr.png")
+    cvQR = cv2.imread("tmp/qr.png")
+    os.remove("tmp/qr.png")
+    return cvQR
 
+def percentageBar(value,max):
+    outputString = "["
+    percent = round((value / max) * 100)
 
-for frames in range(0,framerate):
-    img = genQR("{vidqr}")
-    #images.append(img)
+    terminalWidth = os.get_terminal_size().columns - 10
 
-#img.save("data/img_" + str(count) + ".png")
-metadata = '{"e":"' + extension + '","v":' + str(version) + ',"p":' + str(len(splitValues)) + ',"f":' + str(framerate) + ',"s":' + str(originalSize) + ',"t":"' + filetype + '"}'
-print("Metadata size: " + str(len(metadata)) + " Bytes")
-if (len(metadata) > bytesPerCode):
-    print("Error creating QR code. Try to increase the version or lower error correction")
-    sys.exit()
+    if terminalWidth > 100:
+        terminalWidth = 100
 
-print(metadata)
-img = genQR(metadata)
-#images.append(img)
+    ticks = round(percent / (100 / terminalWidth))
+    for index in range(0,terminalWidth):
+        if index == round(terminalWidth / 2):
+            outputString = outputString + " " + str(percent) + "% "
+        elif index < ticks:
+            outputString = outputString + "*"
+        else:
+            outputString = outputString + " "
+    outputString = outputString + "] "
+    return outputString
 
+def generateCodesFromArray(dataArray):
+    global numberOfCodes
+    newImageArray = []
+    for dataIndex in range(0,len(dataArray)):
+        clearConsole()
+        print("generating packets...")
+        print(percentageBar(dataIndex + 1,len(dataArray)))
+        print(str(dataIndex + 1) + " of " + str(len(dataArray)))
 
+        #Create 4-byte (32-bit int) header for packet ID and encode to Base64
+        header = binascii.b2a_base64((dataIndex + 1).to_bytes(4, byteorder = 'big'))[:-1]
 
-count = 0
-for splitValue in splitValues:
-    count += 1
-    print("\033c")
-    print(percentageBar(count,len(splitValues)))
-    print(str(count) + " of " + str(numberOfCodes))
-    #print(str(count) + ":" + splitValue)
-    #print("")
-    header = binascii.b2a_base64(count.to_bytes(4, byteorder = 'big'))[:-1]
-    print(binascii.a2b_base64(header))
-    img = genQR(header + splitValue)
-    images.append(img)
-    #cv2.imwrite("data/img_" + str(count) + ".png", img)
-    #img.save("data/img_" + str(count) + ".png")
+        #Generate QR code with header and file data
+        img = generateQR(header + dataArray[dataIndex])
+        newImageArray.append(img)
+    return newImageArray
 
-count = 0
-for image in images:
-    count += 1
-    image.save("tmp/" + str(count) + ".png")
-    barcodeOfInt(count)
+def includeMetadataBarcode(qrCode, barCode, interpolation=cv2.INTER_CUBIC):
+    qrHeight = qrCode.shape[0]
+    qrWidth = qrCode.shape[1]
 
+    #bcHeight * (qrWidth / bcHeight) = height scaled proportionally to new width
+    bcHeight = int(barCode.shape[1] * (barCode.shape[0] / qrWidth) / 2)
 
-if args.flash == "true":
-    count += 1
-    img = cv2.imread('tmp/1.png')
-    h, w, c = img.shape
+    #Resize barcode to ensure its width matches the qr code height
+    barCode = cv2.resize(barCode, (qrWidth,bcHeight), interpolation=cv2.INTER_CUBIC)
+
+    #qrCode = cv2.resize(qrCode, (500,500), interpolation=cv2.INTER_CUBIC)
+    #barCode = cv2.resize(barCode, (500,100), interpolation=cv2.INTER_CUBIC)
+
+    return cv2.vconcat([qrCode,barCode])
+
+def generateBlankImage(h, w):
     a = np.full((h, w, 3), 255, dtype=np.uint8)
     image = Image.fromarray(a, "RGB")
-    image.save("tmp/" + str(count) + ".png", "PNG")
+    return image
 
-count += 1
+def imageIOfromCV2(cv2Image):
+    cv2.imwrite("tmp/cv2.png",cv2Image)
+    ioImage = imageio.imread("tmp/cv2.png")
+    os.remove("tmp/cv2.png")
+    return ioImage
 
-images = []
 
-for index in range(1,count):
+#Clear display but leave permanent text if desired
+def clearConsole(includePermanent=True):
+    global permanentPrint
     print("\033c")
-    print("loaded " + str(index) + " of " + str(count))
-    cv2.imwrite("tmp/new_" + str(index) + ".png",joinImages(cv2.imread("tmp/" + str(index) + ".png"),barcode))
-    image = imageio.imread("tmp/new_" + str(index) + ".png")
-    images.append(image)
+    if includePermanent == True:
+        print(permanentPrint)
+        print("--------------------")
 
-print("\033c")
-print("generating data.gif - " + str(count))
-imageio.mimsave('data/data_v' + str(versionOfCode) + '.gif', images, fps=framerate)
-duration = time.time() - startTime
-totalSecs = round(duration,1)
-transferSpeed = round(originalSize / totalSecs)
-print("Duration: " + str(totalSecs) + " seconds")
-print("Rate: " + str(transferSpeed) + " Bytes/sec")
+def encode(inputPath,outputPath):
+    global flashes, versionOfCode, errorCorrection, framerate, extension, originalSize, permanentPrint
+    bin_data = open(inputPath, 'rb').read() #read file as binary
+    paragraph = binascii.b2a_base64(bin_data) #convert binary to Base64 string
+
+    #Save copy of Base64 encoded binary for debugging purposes
+    file = open('tmp/bin.txt', "wb")
+    file.write(paragraph)
+    file.close()
+
+    #Calculate number of codes that need to be generated, taking size of header into account
+    bytesPerCode = capacity[versionOfCode - 1][errorCorrection] - 8
+    totalBytes = len(paragraph)
+    numberOfCodes = math.ceil(totalBytes / bytesPerCode)
+
+    permanentPrint = "File size: " + str(totalBytes) + " Bytes\nSize per Code: " + str(bytesPerCode) + " Bytes\nNumber of Codes: " + str(numberOfCodes)
+    clearConsole()
+
+    #Split file data into array of strings which is used to generate series of QR codes
+    splitValues = [paragraph[i: i + bytesPerCode] for i in range(0, len(paragraph), bytesPerCode)]
+    images = generateCodesFromArray(splitValues)
+
+    permanentPrint = permanentPrint + "\n--------------------\ngenerating packets...done"
+    clearConsole()
+
+    #Add blank image to series so that the GIF animation flashes between every loop
+    if flashes:
+        #Create blank image with same dimensions as first image in packet array
+        images.append(generateBlankImage(images[0].shape[0],images[0].shape[1]))
+
+    #Final string stored in barcode
+    metadata = str(framerate) + ":" + str(len(splitValues)) + ":" + extension + ":" + str(originalSize)
+    metadataBarcode = generateCode128(metadata)
+
+    #Add metadata barcode under each QR code and convert to imageIO (required format for making gif)
+    gifImages = []
+    for imageIndex in range(0,len(images)):
+        clearConsole()
+        print("processing images...")
+        print(percentageBar(imageIndex + 1,len(images)))
+        print(str(imageIndex + 1) + " of " + str(len(images)))
+        if imageIndex == len(images) - 1:
+            permanentPrint = permanentPrint + "\nprocessing images...done"
+        gifImages.append(imageIOfromCV2(includeMetadataBarcode(images[imageIndex],metadataBarcode)))
+
+    #Show calculation of roughly estimated time (based on my own tests) to generate GIF
+    clearConsole()
+    print("stitching vidQR code...")
+    estimatedSeconds = round(len(gifImages) / 45)
+    if estimatedSeconds > 1:
+        print("This should take about " + str(estimatedSeconds) + " seconds")
+    else:
+        print("This should take just a second")
+
+    #Create gif from array of images
+    imageio.mimsave(outputPath, gifImages, fps=framerate)
+
+    permanentPrint = permanentPrint + "\nstitching vidQR code...done"
+
+    clearConsole()
+    print("Saved vidQR code to " + os.getcwd() + "/" + outputPath)
+    print("--------------------")
+
+
+encode(args.path,args.output)
